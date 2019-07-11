7d33d5a2c76a4d373e5c787fd5e37d6daf747ae9
Гладков Максим <mgladkov@abz-1.ru>
2019-07-11 15:59:41 +0300

    start routing

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
 'use strict';
 
-/*
-    Параметры командной строки DEMO режим
-    --demo, --demo=true/false - включает выключает демо режим
-    --data-transfer-period=10 - период передачи данных трекером в секундах. по умолчанию 10
-*/
-const DraftLog = require('draftlog')
-    , config   = require('./config');
+const path         = require('path')
+    , store        = require('./store')
+    , config       = require('./config')
+    , express      = require('express')
+    , session      = require('express-session')
+    , bodyParser   = require('body-parser')
+    , cookieParser = require('cookie-parser')
+    , app          = express();
 
-DraftLog(console);
+app.use('/js', browserify(__dirname + '/public/js'));
+app.use('/css', browserify(__dirname + '/public/css'));
+app.use('/lib', express.static(path.join(__dirname, './public/lib')));
 
-if(config.get('demo') === true) {
-    require('./demo')();
-}
+app.engine('.html', require('ejs').__express);
+app.set('views', path.join(__dirname, './public/view'));
+app.set('view engine', 'html');
 
-process.stdin.resume();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
+app.use(cookieParser());
+app.use(session(config.get('session')));
 
+app.listen(config.get('base:port'), function () {
+  log.info('Сервер запущен. Порт: %s', config.get('base:port'));
+});
